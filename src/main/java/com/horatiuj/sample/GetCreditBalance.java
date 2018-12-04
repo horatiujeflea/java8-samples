4ecb5f8a948c9eb7b9cf0068dc8729629e059576
Horatiu Jeflea <horatiu.jeflea@gmail.com>
2018-12-04 14:49:33 +0200

    Add credit balance

---
+++ src/main/java/com/horatiuj/sample/GetCreditBalance.java
@@ -0,0 +1,10 @@
+package com.horatiuj.sample;
+
+import java.util.function.Function;
+
+public class GetCreditBalance implements Function<String, String> {
+    @Override
+    public String apply(String s) {
+        return null;
+    }
+}
