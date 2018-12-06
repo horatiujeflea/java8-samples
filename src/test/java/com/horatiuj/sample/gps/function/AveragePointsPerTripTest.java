81dc025478407550d1ce26aade6a1bdf58bd6da2
Horatiu Jeflea <horatiu.jeflea@gmail.com>
2018-12-06 20:06:11 +0200

    Add gps probe function test refactor

--- src/test/java/com/horatiuj/sample/gps/function/AveragePointsPerTripTest.java
+++ src/test/java/com/horatiuj/sample/gps/function/AveragePointsPerTripTest.java
@@ -1,18 +1,13 @@
 package com.horatiuj.sample.gps.function;
 
-import com.horatiuj.sample.gps.model.ImmutableGpsPoint;
-import com.horatiuj.sample.gps.model.ImmutableTrip;
 import com.horatiuj.sample.gps.model.Trip;
+import com.horatiuj.sample.gps.util.TripGenerator;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.OptionalDouble;
-import java.util.UUID;
-
-import static com.horatiuj.sample.gps.util.TestUtil.stringToDate;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AveragePointsPerTripTest {
 
@@ -22,53 +17,8 @@ class AveragePointsPerTripTest {
 
         ArrayList<Trip> trips = new ArrayList<>();
 
-        String tripId = UUID.randomUUID().toString();
-        Double lat = 46.775004;
-        Double lon = 23.587317;
-
-        trips.add(ImmutableTrip.builder()
-                .id(tripId)
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:00:00.000-00:00"))
-                        .lat(lat)
-                        .lon(lon)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:01:00.000-00:00"))
-                        .lat(lat)
-                        .lon(lon)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:01:30.000-00:00"))
-                        .lat(lat)
-                        .lon(lon)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:01:45.400-00:00"))
-                        .lat(lat)
-                        .lon(lon)
-                        .build())
-                .build());
-
-        trips.add(ImmutableTrip.builder()
-                .id(tripId)
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:10.000-00:00"))
-                        .lat(lat)
-                        .lon(lon)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:18:00.000-00:00"))
-                        .lat(lat)
-                        .lon(lon)
-                        .build())
-                .build());
+        trips.add(TripGenerator.sampleTrip1());
+        trips.add(TripGenerator.sampleTrip2());
 
         OptionalDouble result = f.apply(trips);
         Assert.assertEquals(3.0d, result.getAsDouble(), 0.01);
--- src/test/java/com/horatiuj/sample/gps/function/AverageSpeedTest.java
+++ src/test/java/com/horatiuj/sample/gps/function/AverageSpeedTest.java
@@ -1,20 +1,14 @@
 package com.horatiuj.sample.gps.function;
 
-import com.horatiuj.sample.gps.model.ImmutableGpsPoint;
-import com.horatiuj.sample.gps.model.ImmutableTrip;
 import com.horatiuj.sample.gps.model.Trip;
+import com.horatiuj.sample.gps.util.TripGenerator;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.OptionalDouble;
-import java.util.UUID;
-
-import static com.horatiuj.sample.gps.util.TestUtil.stringToDate;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AverageSpeedTest {
 
@@ -29,34 +23,7 @@ class AverageSpeedTest {
     void oneTripTest() throws ParseException {
         ArrayList<Trip> trips = new ArrayList<>();
 
-        String tripId = UUID.randomUUID().toString();
-        trips.add(ImmutableTrip.builder()
-                .id(tripId)
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:10.000-00:00"))
-                        .lat(46.775004)
-                        .lon(23.587317)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:17.000-00:00"))
-                        .lat(46.775502)
-                        .lon(23.587261)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:20.000-00:00"))
-                        .lat(46.776195)
-                        .lon(23.587201)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:22.000-00:00"))
-                        .lat(46.776384)
-                        .lon(23.587180)
-                        .build())
-                .build());
+        trips.add(TripGenerator.sampleTrip1());
 
         OptionalDouble result = f.apply(trips);
         Assert.assertEquals(12.81d, result.getAsDouble(), 0.01);
@@ -66,50 +33,8 @@ class AverageSpeedTest {
     void multipleTripsTest() throws ParseException {
         ArrayList<Trip> trips = new ArrayList<>();
 
-        String tripId = UUID.randomUUID().toString();
-        trips.add(ImmutableTrip.builder()
-                .id(tripId)
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:10.000-00:00"))
-                        .lat(46.775004)
-                        .lon(23.587317)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:17.000-00:00"))
-                        .lat(46.775502)
-                        .lon(23.587261)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:20.000-00:00"))
-                        .lat(46.776195)
-                        .lon(23.587201)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:22.000-00:00"))
-                        .lat(46.776384)
-                        .lon(23.587180)
-                        .build())
-                .build());
-
-        trips.add(ImmutableTrip.builder()
-                .id(tripId)
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:10.000-00:00"))
-                        .lat(46.776855)
-                        .lon(23.607117)
-                        .build())
-                .addPoints(ImmutableGpsPoint.builder()
-                        .trip(tripId)
-                        .timestamp(stringToDate("2018-12-03T08:08:20.000-00:00"))
-                        .lat(46.780095)
-                        .lon(23.624429)
-                        .build())
-                .build());
+        trips.add(TripGenerator.sampleTrip1());
+        trips.add(TripGenerator.sampleTrip2());
 
         OptionalDouble result = f.apply(trips);
         Assert.assertEquals(69.11d, result.getAsDouble(), 0.01);
